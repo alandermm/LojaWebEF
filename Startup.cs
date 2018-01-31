5ad0c410d933e10476c3ffb6bceab90589313533
Alander Machado <alandermm@hotmail.com>
2018-01-31 21:51:55 -0200

    OK

--- Startup.cs
+++ Startup.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaWebEF.Dados;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LojaWebEF
 {
     public class Startup
     {
+        IConfiguration configuration{get; set;}
+        public Startup(IConfiguration configuration){
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<LojaContexto>(options=>options.UseSqlServer(configuration.GetConnectionString("BancoLojaEF")));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +33,8 @@ namespace LojaWebEF
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
